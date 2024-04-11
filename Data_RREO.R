8119c27bd8a59753229ad47944062cfe910f056a
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2024-04-11 19:55:50 -0300

    Estruturação da consulta a API

--- Data_RREO.R
+++ Data_RREO.R
@@ -2,23 +2,40 @@
 # === COLETA E ARMAZENAMENTO DOS DADOS DA API === #
 # =============================================== #
 
+# --- Script by Paulo Icaro ---#
 
+
+
+# ------------------------ #
+# -- Funcoes Auxiliares -- #
+# ------------------------ #
 source('FG_URL_RREO.R')
 source('API_Siconfi.R')
 
 
-
-
+# ---------------- #
+# -- Parametros -- #
+# ---------------- #
 ano = 2018:2023
 bimestre = 1:6
 ente = 43
-relatorio = 01
+relatorio = '01'
 esfera = 'E'
 tipo_demonstrativo = 'RREO'
 
 
 
+# -------------------------------- #
+# -- Variaveis de Armazenamento -- #
+# -------------------------------- #
 rreo_url = NULL
+dataset_rreo = NULL
+
+
+
+# ----------------------- #
+# -- Geracao das URL's -- #
+# ----------------------- #
 for (i in ano){
   for (j in ente){
     for (x in bimestre){
@@ -27,4 +44,15 @@ for (i in ano){
       }
     }
   }
+}
+
+
+
+# ------------------------ #
+# -- Extracao dos Dados -- # 
+# ------------------------ #
+for (w in 1:length(rreo_url)){
+  dataset_rreo = rbind(dataset_rreo, siconfi_api(url = rreo_url[w]))
+  Sys.sleep(3)
+  message(paste(w, 'download(s) de', length(rreo_url), ' \n', sep = ' '))
 }
