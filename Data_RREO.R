d0d8b3c53ebf8ad4a13c9da27b4ad23904df9ece
pauloicaro <pauloicaro38@gmail.com>
2024-04-14 21:23:10 -0300

    Adição de script para separação das rubricas desejadas

--- Data_RREO.R
+++ Data_RREO.R
@@ -19,7 +19,7 @@ source('API_Siconfi.R')
 ano = 2018:2023
 bimestre = 1:6
 ente = 43
-relatorio = '01'
+relatorio = c('01', '03', '04', '06')
 esfera = 'E'
 tipo_demonstrativo = 'RREO'
 
@@ -40,7 +40,7 @@ for (i in ano){
   for (j in ente){
     for (x in bimestre){
       for (y in relatorio){
-         rreo_url = rbind(rreo_url, param_rreo(i, x, tipo_demonstrativo, relatorio, esfera, j))
+         rreo_url = rbind(rreo_url, param_rreo(i, x, tipo_demonstrativo, y, esfera, j))
       }
     }
   }
@@ -53,6 +53,19 @@ for (i in ano){
 # ------------------------ #
 for (w in 1:length(rreo_url)){
   dataset_rreo = rbind(dataset_rreo, siconfi_api(url = rreo_url[w]))
-  Sys.sleep(3)
+  Sys.sleep(2)
   message(paste(w, 'download(s) de', length(rreo_url), ' \n', sep = ' '))
+  if(w == length(rreo_url)){message('Todos os dados solicitados foram coletados !')}
 }
+
+
+# -------------- #
+# -- Rubricas -- #
+# -------------- #
+receitas_correntes = dataset_rreo |> filter(cod_conta == 'ReceitasCorrentes')
+receitas_capital = dataset_rreo |> filter(cod_conta == 'ReceitasDeCapital')
+operacoes_credito = dataset_rreo |> filter(cod_conta == 'ReceitasDeOperacoesDeCredito')
+investimentos_pagos = dataset_rreo |> filter(cod_conta == 'Investimentos')
+inversoes_financeiras_pagas = dataset_rreo |> filter(cod_conta == 'InversoesFinanceiras')
+despesas_correntes_pagas = dataset_rreo |> filter(cod_conta == 'DespesasCorrentes')
+juros_pagos = dataset_rreo |> filter(cod_conta == 'JurosEEncargosDaDivida')
