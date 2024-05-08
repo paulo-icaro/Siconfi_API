d7633a4a3ebe46e9eea95baf3e19cfd28003aed0
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2024-05-08 09:19:12 -0300

    Atualização da nomenclatura da função

--- FG_URL_RREO.R
+++ FG_URL_RREO.R
@@ -9,7 +9,7 @@
 # ------------------------------ #
 # --- Funcao Geradora de URL --- #
 # ------------------------------ #
-param_rreo = function(an_exercicio, nr_periodo, co_tipo_demonstrativo, no_anexo, co_esfera, id_ente){
+url_rreo = function(an_exercicio, nr_periodo, co_tipo_demonstrativo, no_anexo, co_esfera, id_ente){
   url = 'http://apidatalake.tesouro.gov.br/ords/siconfi/tt/rreo'
   
   for(i in an_exercicio){
