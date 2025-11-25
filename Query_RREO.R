f64963db6523931b7e1dbb217275d9b30a1ef6da
Paulo <pauloicaro38@gmail.com>
2025-11-24 21:29:39 -0300

    Versão inicial do script de consulta

---
+++ Query_RREO.R
@@ -0,0 +1,47 @@
+# ========================== #
+# === SCRIPT DE CONSULTA === #
+# ========================== #
+
+# --- Script by Paulo Icaro --- #
+
+
+# ========================= #
+# === Query de Consulta === #
+# ========================= #
+
+query_rreo = function(ano, periodo, co_tipo_demonstrativo, anexo = NULL, esfera = NULL, ente, remove_url = TRUE){
+  
+  # -------------------------- #
+  # --- Funcoes Auxiliares ---#
+  # -------------------------- #
+  source('https://raw.githubusercontent.com/paulo-icaro/Siconfi_API/refs/heads/main/API_Siconfi.R')
+  source('https://raw.githubusercontent.com/paulo-icaro/Siconfi_API/refs/heads/main/FG_URL_RREO.R')
+  
+  # ---------------------------------- #
+  # --- Variáveis de Armazenamento --- #
+  # ---------------------------------- #
+  rreo_url = dataset_rreo = NULL
+  
+  # ------------------------- #
+  # --- Construção de URL --- #
+  # ------------------------- #
+  for (i in ano){
+    for (j in ente){
+      for (x in bimestre){
+        for (y in relatorio){
+          rreo_url = rbind(rreo_url, url_rreo(i, x, tipo_demonstrativo, y, esfera, j))
+        }
+      }
+    }
+  }
+  
+  # ------------------------ #
+  # -- Extracao dos Dados -- # 
+  # ------------------------ #
+  for (w in 1:length(rreo_url)){
+    dataset_rreo = rbind(dataset_rreo, siconfi_api(url = rreo_url[w]))
+    Sys.sleep(1.5)
+    message(paste(w, 'download(s) de', length(rreo_url), ' \n', sep = ' '))
+    if(w == length(rreo_url)){message('Todos os dados solicitados foram coletados !')}
+  }  
+}
