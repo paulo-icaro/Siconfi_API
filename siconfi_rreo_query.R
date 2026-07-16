3364fd8dcd680437e888d37ba0e86ca5af5bfbd8
paulo-icaro <pauloicaro38@gmail.com>
2026-07-16 10:44:47 -0300

    Ajuste do nome das funções auxiliares na rotina

--- siconfi_rreo_query.R
+++ siconfi_rreo_query.R
@@ -31,7 +31,7 @@ siconfi_rreo_query = function(ano, periodo, co_tipo_demonstrativo, relatorio = N
     for (j in ente){
       for (x in periodo){
         for (y in relatorio){
-          rreo_url = rbind(rreo_url, url_rreo(i, x, co_tipo_demonstrativo, y, esfera, j))
+          rreo_url = rbind(rreo_url, siconfi_rreo_url(i, x, co_tipo_demonstrativo, y, esfera, j))
         }
       }
     }
@@ -41,7 +41,7 @@ siconfi_rreo_query = function(ano, periodo, co_tipo_demonstrativo, relatorio = N
   # -- Extracao dos Dados -- # 
   # ------------------------ #
   for (w in 1:length(rreo_url)){
-    dataset_rreo = rbind(dataset_rreo, siconfi_api(url = rreo_url[w]))
+    dataset_rreo = rbind(dataset_rreo, siconfi_rreo_api(url = rreo_url[w]))
     Sys.sleep(1)
     message(paste(w, 'download(s) de', length(rreo_url), ' \n', sep = ' '))
     if(w == length(rreo_url)){message('Todos os dados solicitados foram coletados !')}
